9cb30c0d66b7061d51b8872340c5cf1149b9a989
Dwija <dwijayaaa21@gmail.com>
2026-01-16 13:14:41 +0800

    update random blog script

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const blogData = [
     id: 2,
     title: "Love Chess",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam numquam corrupti in optio illo, porro fugit reprehenderit? Itaque qui maiores, quibusdam ipsum libero veritatis!",
-    image: "https://images.unsplash.com/photo-1529699211952-734e80c4d42b?w=800&h=500&fit=crop",
+    image: "https://images.pexels.com/photos/6114964/pexels-photo-6114964.jpeg",
     category: "Sport",
     date: "1 Jan 2067",
     author: "Dwija",
@@ -50,7 +50,7 @@ const blogData = [
     id: 3,
     title: "Like Chess",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam numquam corrupti in optio illo, porro fugit reprehenderit? Itaque qui maiores, quibusdam ipsum libero veritatis!",
-    image: "img/chess.jpg",
+    image: "https://images.pexels.com/photos/8438957/pexels-photo-8438957.jpeg",
     category: "Sport",
     date: "1 Jan 2067",
     author: "Dwija",
@@ -59,7 +59,7 @@ const blogData = [
     id: 4,
     title: "Why Chess ?",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam numquam corrupti in optio illo, porro fugit reprehenderit? Itaque qui maiores, quibusdam ipsum libero veritatis!",
-    image: "img/chess.jpg",
+    image: "https://images.pexels.com/photos/27680788/pexels-photo-27680788.jpeg",
     category: "Sport",
     date: "1 Jan 2067",
     author: "Dwija",
@@ -68,7 +68,7 @@ const blogData = [
     id: 5,
     title: "Chess is Fun",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam numquam corrupti in optio illo, porro fugit reprehenderit? Itaque qui maiores, quibusdam ipsum libero veritatis!",
-    image: "https://images.unsplash.com/photo-1529699211952-734e80c4d42b?w=800&h=500&fit=crop",
+    image: "https://images.pexels.com/photos/28397731/pexels-photo-28397731.jpeg",
     category: "Sport",
     date: "1 Jan 2067",
     author: "Dwija",
@@ -77,7 +77,7 @@ const blogData = [
     id: 6,
     title: "Chess is Cool",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam numquam corrupti in optio illo, porro fugit reprehenderit? Itaque qui maiores, quibusdam ipsum libero veritatis!",
-    image: "img/chess.jpg",
+    image: "https://images.pexels.com/photos/6598746/pexels-photo-6598746.jpeg",
     category: "Sport",
     date: "1 Jan 2067",
     author: "Dwija",
@@ -86,7 +86,7 @@ const blogData = [
     id: 7,
     title: "Chess is my life",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam numquam corrupti in optio illo, porro fugit reprehenderit? Itaque qui maiores, quibusdam ipsum libero veritatis!",
-    image: "https://images.unsplash.com/photo-1529699211952-734e80c4d42b?w=800&h=500&fit=crop",
+    image: "https://images.pexels.com/photos/6114982/pexels-photo-6114982.jpeg",
     category: "Sport",
     date: "1 Jan 2067",
     author: "Dwija",
@@ -95,7 +95,7 @@ const blogData = [
     id: 8,
     title: "Chess is my passion",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam numquam corrupti in optio illo, porro fugit reprehenderit? Itaque qui maiores, quibusdam ipsum libero veritatis!",
-    image: "img/chess.jpg",
+    image: "https://images.pexels.com/photos/8998424/pexels-photo-8998424.jpeg",
     category: "Sport",
     date: "1 Jan 2067",
     author: "Dwija",
@@ -104,7 +104,7 @@ const blogData = [
     id: 9,
     title: "Chess is my hobby",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam numquam corrupti in optio illo, porro fugit reprehenderit? Itaque qui maiores, quibusdam ipsum libero veritatis!",
-    image: "https://images.unsplash.com/photo-1529699211952-734e80c4d42b?w=800&h=500&fit=crop",
+    image: "https://images.pexels.com/photos/14565175/pexels-photo-14565175.jpeg",
     category: "Sport",
     date: "1 Jan 2067",
     author: "Dwija",
@@ -113,7 +113,7 @@ const blogData = [
     id: 10,
     title: "Chess is my favorite game",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam numquam corrupti in optio illo, porro fugit reprehenderit? Itaque qui maiores, quibusdam ipsum libero veritatis!",
-    image: "img/chess.jpg",
+    image: "https://images.pexels.com/photos/6817700/pexels-photo-6817700.jpeg",
     category: "Sport",
     date: "1 Jan 2067",
     author: "Dwija",
@@ -122,7 +122,7 @@ const blogData = [
     id: 11,
     title: "Magnus Carlsen",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam numquam corrupti in optio illo, porro fugit reprehenderit? Itaque qui maiores, quibusdam ipsum libero veritatis!",
-    image: "https://images.unsplash.com/photo-1529699211952-734e80c4d42b?w=800&h=500&fit=crop",
+    image: "https://img.okezone.com/content/2025/06/04/65/3144582/segini_iq_raja_catur_dunia_magnus_carlsen_yang_baru_saja_dikalahkan_pecatur_muda_gukesh_dommaraju-I6w9_large.jpg",
     category: "Sport",
     date: "1 Jan 2067",
     author: "Dwija",
@@ -131,7 +131,7 @@ const blogData = [
     id: 12,
     title: "Hikaru Nakamura",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam numquam corrupti in optio illo, porro fugit reprehenderit? Itaque qui maiores, quibusdam ipsum libero veritatis!",
-    image: "img/chess.jpg",
+    image: "https://en.chessbase.com/thumb/89875",
     category: "Sport",
     date: "1 Jan 2067",
     author: "Dwija",
@@ -171,6 +171,14 @@ function renderPosts(page) {
 
   renderPagination()
 }
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+shuffleArray(blogData);
 
 function renderPagination() {
   const totalPages = Math.ceil(blogData.length / postsPerPage);
